7fa9d59a2e19264d22f70dcb28519f7758eff1e9
David Dryburgh <david.dryburgh1994@gmail.com>
2018-03-06 16:46:33 +0000

    Init Commit

---
+++ platforms/android/assets/www/cordova_plugins.js
@@ -0,0 +1,18 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+  {
+    "id": "com.wikitude.phonegap.WikitudePlugin.WikitudePlugin",
+    "file": "plugins/com.wikitude.phonegap.WikitudePlugin/www/WikitudePlugin.js",
+    "pluginId": "com.wikitude.phonegap.WikitudePlugin",
+    "clobbers": [
+      "WikitudePlugin"
+    ]
+  }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+  "com.wikitude.phonegap.WikitudePlugin": "7.2.1"
+};
+// BOTTOM OF METADATA
+});
